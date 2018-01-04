7ac78a4ad578833d03a9c8a63ef27ecded8d1ad5
Jagrav Naik <jagravnaik0@gmail.com>
2018-01-04 09:21:45 -0500

    oneplus3: pixel-ify
    Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>

--- aosp_oneplus3.mk
+++ aosp_oneplus3.mk
@@ -21,9 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosip_oneplus3
+PRODUCT_NAME := aosp_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -31,3 +31,6 @@ PRODUCT_BRAND := OnePlus
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 TARGET_VENDOR := oneplus
+TARGET_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_DENSITY := xxxhdpi
